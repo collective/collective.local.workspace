0e0abfae4678ad7daafdc1eaa550fff1b947d844
Thomas Desvenain <thomasdesvenain@ecreall.com>
2013-07-22 19:10:59 +0200

    add an interface to request if we are in a workspace

--- src/collective/local/workspace/interfaces.py
+++ src/collective/local/workspace/interfaces.py
@@ -3,3 +3,7 @@ from zope.interface import Interface
 class IWorkspace(Interface):
     """
     """
+    
+class IWorkspaceLayer(Interface):
+    """Layer that is activated when we are in a workspace
+    """
