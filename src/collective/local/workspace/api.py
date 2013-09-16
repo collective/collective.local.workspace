b991a517bbdfef89839029619970c5a4a3c36518
Thomas Desvenain <thomas.desvenain@gmail.com>
2013-09-16 10:08:44 +0200

    fix getting a workspace included into a workspace

--- src/collective/local/workspace/api.py
+++ src/collective/local/workspace/api.py
@@ -2,7 +2,7 @@ from Acquisition import aq_chain
 from collective.local.workspace.interfaces import IWorkspace
 
 def get_workspace(obj):
-    for item in reversed(aq_chain(obj)):
+    for item in aq_chain(obj):
         if IWorkspace.providedBy(item):
             return item
     else:
